8b9ca98d378eb675362c75dc92c2effa4cb0c7a2
Marko Brica <marko.brica@gmail.com>
2018-05-11 17:17:42 +0200

    Dodan adapter za prikaz kategorija u admin dijelu aplikacije

--- app/src/main/java/com/example/marko/blagajnapp/TabKategorije.java
+++ app/src/main/java/com/example/marko/blagajnapp/TabKategorije.java
@@ -1,14 +1,19 @@
 package com.example.marko.blagajnapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class TabKategorije extends Fragment {
 
@@ -17,13 +22,24 @@ public class TabKategorije extends Fragment {
     private TextView tvkategorije;
     private FloatingActionButton fabNovaKategorija;
     private RecyclerView rvPrikazKategorija;
+    private PrikazKategorijeAdapter mPrikazKategorijaAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.ItemDecoration mItemDecoration;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_kategorije,container,false);
         TextView tvkategorije = (TextView) view.findViewById(R.id.tvdjelatnici);
 
-        //dodati RecyclerView
+        Context context = getActivity();
+        rvPrikazKategorija = (RecyclerView) view.findViewById(R.id.rvPopisKategorija);
+        mPrikazKategorijaAdapter = new PrikazKategorijeAdapter(new ArrayList<Kategorija>());
+        mLayoutManager = new LinearLayoutManager(context);
+        mItemDecoration = new DividerItemDecoration(context,DividerItemDecoration.VERTICAL);
+
+        rvPrikazKategorija.addItemDecoration(mItemDecoration);
+        rvPrikazKategorija.setLayoutManager(mLayoutManager);
+        rvPrikazKategorija.setAdapter(mPrikazKategorijaAdapter);
 
         FloatingActionButton fabNovaKategorija = (FloatingActionButton) view.findViewById(R.id.fabNovaKategorija);
         fabNovaKategorija.setOnClickListener(new View.OnClickListener() {
